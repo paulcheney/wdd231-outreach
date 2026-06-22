40ce68b6d14f1603b3c88023acc2ba8a4fb9d15a
Paul Cheney <spartandesign.biz@gmail.com>
2026-06-21 19:33:09 -0600

    sFG

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //Set the date below to the first Monday of the Pathway term
 const termStart = new Date('6/29/2026').getTime()+43200000; //add 12 hours in case daylight savings in in effect.
 //console.log(termStart)
-const termNumber = '2026_3'
+const termNumber = '2026_4'
 const monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const dayList = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 
